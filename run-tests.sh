496ae54ed93fbaf972f162a42e81ffbfcf345786
Francis Avila <francis@shortcut.com>
2023-01-05 13:17:26 -0600

    Add latest-datomic-pro alias and exercise in tests
    Adds latest-datomic-pro alias with the datomic-pro 1.0.6527 dependency.
    
    This dependency requires a licence and an authenticated repository,
    so it cannot be in the run-test.sh test matrix;
    but a commented-out line was left for those who have it.
    
    Also removes the clojure6 test matrix item from run-tests.sh
    because that alias was removed in v0.2.2.

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,8 @@
 lein with-profile test,last-clojure,oldest-datomic test
 lein with-profile test,last-clojure,last-datomic test
 
-lein with-profile test,clojure7,oldest-datomic test
+# Uncomment if you have a datomic-pro license
+# and access to the dependency via maven.
+#lein with-profile test,last-clojure,last-datomic-pro test
 
-lein with-profile test,clojure6,oldest-datomic test
+lein with-profile test,clojure7,oldest-datomic test
